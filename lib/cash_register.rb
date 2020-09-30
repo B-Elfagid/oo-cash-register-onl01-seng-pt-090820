c5b9c246fd59c844c40cf1b38ab0dbf89c599ded
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-09-30 20:44:00 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,6 +9,21 @@
     @total = 0 
   end 
    
+    def add_item(title,price,quantity = 1)
+    if quantity>1
+      i=0
+      while i<quantity
+        @items << title
+        i+=1
+      end
+    else
+      @items << title
+    end
+    @last_total = @total
+    @total += price*quantity
+    @last_transaction_amount = @total
+    @total
+  end
   
   
   
