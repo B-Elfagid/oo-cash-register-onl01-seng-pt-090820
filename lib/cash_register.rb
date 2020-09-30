b99ca4fa1f8a8456d3a9b1754324f0e79de8e121
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-09-30 20:41:44 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,9 +1,10 @@
   class CashRegister 
+    
   attr_accessor :discount, :items, :last_transaction, :total
 
 
   def initialize(discount = 0)
-    #@items = []
+    @items = []
     @discount = discount
     @total = 0 
   end 
